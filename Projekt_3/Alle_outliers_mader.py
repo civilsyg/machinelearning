4d225be22f52b048da7cee7b3c4a77dffb7c7871
Iben <iben95@hotmail.dk>
2018-04-23 09:34:38 +0200

    alle outliers

---
+++ Projekt_3/Alle_outliers_mader.py
@@ -0,0 +1,128 @@
+# exercise 11.4.1
+import numpy as np
+from matplotlib.pyplot import (figure, imshow, bar, title, xticks, yticks, cm,
+                               subplot, show)
+from scipy.io import loadmat
+from toolbox_02450 import gausKernelDensity
+from sklearn.neighbors import NearestNeighbors
+from projekt3 import pimaData, X
+from scipy import stats
+
+
+y = np.array(pimaData[['classVariable']]) # real prediction 
+
+attributeNames = [
+    'pregnant',
+    'glucose',
+    'bloodPressure',
+    'skinThickness',
+    'bodyMass',
+    'pedigreeFunction',
+    'age'
+    ]
+
+N, M = X.shape
+C = 2
+# Normalize data
+X = stats.zscore(X);
+X = np.asmatrix(X);
+# Load Matlab data file and extract variables of interest
+
+classNames = ['Non diabetes', 'Diabetes']
+N, M = X.shape
+C = len(classNames)
+
+
+
+### Gausian Kernel density estimator
+# cross-validate kernel width by leave-one-out-cross-validation
+# (efficient implementation in gausKernelDensity function)
+# evaluate for range of kernel widths
+widths = X.var(axis=0).max() * (2.0**np.arange(-10,3))
+logP = np.zeros(np.size(widths))
+for i,w in enumerate(widths):
+   print('Fold {:2d}, w={:f}'.format(i,w))
+   density, log_density = gausKernelDensity(X,w)
+   logP[i] = log_density.sum()
+   
+val = logP.max()
+ind = logP.argmax()
+
+width=widths[ind]
+print('Optimal estimated width is: {0}'.format(width))
+
+# evaluate density for estimated width
+density, log_density = gausKernelDensity(X,width)
+
+# Sort the densities
+i = (density.argsort(axis=0)).ravel()
+density = density[i].reshape(-1,)
+
+# Plot density estimate of outlier score
+figure(1)
+bar(range(20),density[:20])
+title('Density estimate')
+
+
+
+
+### K-neighbors density estimator
+# Neighbor to use:
+K = 5
+
+# Find the k nearest neighbors
+knn = NearestNeighbors(n_neighbors=K).fit(X)
+D, i = knn.kneighbors(X)
+
+density = 1./(D.sum(axis=1)/K)
+
+# Sort the scores
+i = density.argsort()
+density = density[i]
+
+# Plot k-neighbor estimate of outlier score (distances)
+figure(3)
+bar(range(20),density[:20])
+title('KNN density: Outlier score')
+
+
+
+### K-nearest neigbor average relative density
+# Compute the average relative density
+
+knn = NearestNeighbors(n_neighbors=K).fit(X)
+D, i = knn.kneighbors(X)
+density = 1./(D.sum(axis=1)/K)
+avg_rel_density = density/(density[i[:,1:]].sum(axis=1)/K)
+
+# Sort the avg.rel.densities
+i_avg_rel = avg_rel_density.argsort()
+avg_rel_density = avg_rel_density[i_avg_rel]
+
+# Plot k-neighbor estimate of outlier score (distances)
+figure(5)
+bar(range(20),avg_rel_density[:20])
+title('KNN average relative density: Outlier score')
+
+
+### Distance to 5'th nearest neighbor outlier score
+K = 5
+
+# Find the k nearest neighbors
+knn = NearestNeighbors(n_neighbors=K).fit(X)
+D, i = knn.kneighbors(X)
+
+# Outlier score
+score = D[:,K-1]
+# Sort the scores
+i = score.argsort()
+score = score[i[::-1]]
+
+# Plot k-neighbor estimate of outlier score (distances)
+figure(7)
+bar(range(20),score[:20])
+title('5th neighbor distance: Outlier score')
+
+
+
+print('Ran Exercise 11.4.1')
